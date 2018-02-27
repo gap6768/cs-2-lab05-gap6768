39040c5abdee692c4e6275c882e797d0b943597a
Gino Placella <gap6768@rit.edu>
2018-02-27 12:32:23 -0500

    added tollreport

---
+++ Tolls R Us/src/TollReport.java
@@ -0,0 +1,12 @@
+/**
+ * A class that will hold the main class and run
+ * the program
+ *
+ * @author Gino Placella
+ */
+public class TollReport {
+
+    public static void main(String[] args){
+
+    }
+}
